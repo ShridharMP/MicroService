dd1d4ab53380c25e72810144328a9cbfb5207cab
ShridharMP <mpshridhar2016@gmail.com>
2023-11-28 21:52:43 +0530

    "ReCommit Product Entity"

--- src/main/java/com/patil/software/solutions/ProductService/entity/Product.java
+++ src/main/java/com/patil/software/solutions/ProductService/entity/Product.java
@@ -5,8 +5,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Product {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -14,26 +22,7 @@ public class Product {
 	@Column(name = "PRODUCT_NAME")
 	private String productName;
 	@Column(name = "PRICE")
-	private long price;
+	private double price;
 	@Column(name = "QUANTITY")
 	private long quantity;
-
-	public Product() {
-		super();
-	}
-
-	public Product(long productId, String productName, long price, long quantity) {
-		super();
-		this.productId = productId;
-		this.productName = productName;
-		this.price = price;
-		this.quantity = quantity;
-	}
-
-	@Override
-	public String toString() {
-		return "Product [productId=" + productId + ", productName=" + productName + ", price=" + price + ", quantity="
-				+ quantity + "]";
-	}
-
 }
