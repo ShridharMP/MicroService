513b5f832953e04356307b7b3e42d6b3781f1444
Shridhar M Patil <mpshridhar2016@gmail.com>
2023-11-29 09:44:35 +0530

    Edited online

--- src/main/java/com/patil/software/solutions/ProductService/exception/ProductServiceCustomException.java
+++ src/main/java/com/patil/software/solutions/ProductService/exception/ProductServiceCustomException.java
@@ -4,13 +4,7 @@ import lombok.Data;
 
 @Data
 public class ProductServiceCustomException extends RuntimeException{
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -950437306568833246L;
 	private String errorMessageCode;
-	
 	public ProductServiceCustomException(String message,String errorMessageCode)
 	{
 		super(message);
